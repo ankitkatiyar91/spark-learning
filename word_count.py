4e7294a6e5cfd5ecb8e8e3d8b82aadc2cc0b4516
Ankit Katiyar <ankitkatiyar67@gmail.com>
2022-06-26 20:33:48 +0530

    spark word count with dataframe

--- word_count.py
+++ word_count.py
@@ -1,6 +1,7 @@
 import re
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, Row
 
 from spark_session import get_spark_session
 
@@ -16,5 +17,16 @@ def word_count():
     spark.stop()
 
 
+def word_count2():
+    spark: SparkSession = get_spark_session('word-count')
+    df = spark.sparkContext.textFile('temp/**').flatMap(lambda line: line.split(" ")) \
+        .map(lambda word: re.sub('[^A-Za-z0-9]+', '', word)).map(lambda r: Row(r))
+    df = df.toDF(['word'])
+    print(df.printSchema())
+    # df['count'] = 0
+    print('Top 5\n', df.groupby(['word']).count().collect())
+    spark.stop()
+
+
 if __name__ == '__main__':
-    word_count()
+    word_count2()
